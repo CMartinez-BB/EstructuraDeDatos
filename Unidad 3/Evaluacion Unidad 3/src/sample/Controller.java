0efb8024dc164cfb322623489d5a3437559a99df
CMartinez-BB <71908093+CMartinez-BB@users.noreply.github.com>
2020-12-22 12:51:14 -0700

    Avances de la evaluacion 3
    Proyecto personal para calificar la unidad 3 de estructura de datos

---
+++ Unidad 3/Evaluacion Unidad 3/src/sample/Controller.java
@@ -0,0 +1,54 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    TextField txtUsuario;
+    @FXML
+    PasswordField txtPassword;
+
+    public void login(ActionEvent event){
+        ingrear();
+
+    }
+    public void enter(KeyEvent event){
+       if (event.getCode()== KeyCode.ENTER){
+           ingrear();
+
+       }
+    }
+    public void ingrear(){
+        String u=txtUsuario.getText();
+        String u1=txtPassword.getText();
+        if (u.equals("19CG0063") && u1.equals("201201")){
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("sample2.fxml"));
+                Scene scene=new Scene(root);
+                Main.stage.setScene(scene);
+                Main.stage.setMaximized(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }else{
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Agenta 2020-2021");
+            alert.setContentText("Datos Invalidos");
+            alert.show();
+        }
+    }
+
+
+}
